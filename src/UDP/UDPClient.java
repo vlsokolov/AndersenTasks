7be6723d767dbbe9e03428668153fb269c8e8db4
Vladimir Sokolov <vladimir.sokolov.od@gmail.com>
2016-11-07 10:04:56 +0200

    TCP client-server app

--- src/UDP/UDPClient.java
+++ src/UDP/UDPClient.java
@@ -3,10 +3,9 @@ package UDP;
 import java.io.*;
 import java.net.*;
 
-class UDPClient
-{
-   public static void main(String args[]) throws Exception
-   {
+class UDPClient {
+	
+   public static void main(String args[]) throws Exception {
       BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
       DatagramSocket clientSocket = new DatagramSocket();
       InetAddress IPAddress = InetAddress.getByName("localhost");
--- src/UDP/UDPServer.java
+++ src/UDP/UDPServer.java
@@ -2,10 +2,9 @@ package UDP;
 
 import java.net.*;
 
-class UDPServer
-{
-   public static void main(String args[]) throws Exception
-      {
+class UDPServer {
+	
+   public static void main(String args[]) throws Exception {
          DatagramSocket serverSocket = new DatagramSocket(9876);
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
